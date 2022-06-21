0fae365bd3bfadb5b88118d6c3b218b27d5c6a9e
carbammato <demetrio.marino1985@gmail.com>
2022-06-21 22:39:00 +0200

    router updated

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
 
+from backend.routers import player_router
+
 app = FastAPI()
+
+app.include_router(player_router.router)
--- backend/routers/player_router.py
+++ backend/routers/player_router.py
@@ -1,34 +1,35 @@
 from fastapi import APIRouter
 import backend.BL.player_logic as PlayerLogic
 from backend.models.player import Player
-router = APIRouter()
+
+router = APIRouter(prefix="/players")
 
 
-@router.get("/players/GetAll", tags=["players"])
+@router.get("/GetAll", tags=["players"])
 async def get_all() -> list[Player]:
     return PlayerLogic.get_all()
 
 
-@router.get("/players/GetById/{id}", tags=["players"])
+@router.get("/GetById/{id}", tags=["players"])
 async def get_player_by_id(id: str) -> Player:
     return PlayerLogic.get_player_by_id(id)
 
 
-@router.get("/players/FilterPlayers/{name}", tags=["players"])
+@router.get("/FilterPlayers/{name}", tags=["players"])
 async def filter_players(name: str) -> list[Player]:
     return PlayerLogic.get_players_by_name(name)
 
 
-@router.post("/players/Add")
+@router.post("/Add")
 async def add_player(player: Player) -> None:
     return PlayerLogic.add_player(player)
 
 
-@router.put("/players/update/{id}")
+@router.put("/update/{id}")
 async def update_player(id: str, player: Player) -> None:
     return PlayerLogic.update_player(id, player)
 
 
-@router.delete("/players/delete/{id}")
+@router.delete("/delete/{id}")
 async def delete_player(id: str) -> None:
     return PlayerLogic.delete_player(id)
