909795bc8c325c7a5845a546583522ecc363e144
carbammato <demetrio.marino1985@gmail.com>
2022-06-26 17:57:26 +0200

    more eslint rules

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,21 +1,17 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "@nativescript/angular";
+import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
 import { HomeComponent } from "./components/home/home.component";
 import { ENDPOINT_URL, MOCK_MODE } from "./providers/tokens";
 
 @NgModule({
 	bootstrap: [AppComponent],
-	imports: [NativeScriptModule, AppRoutingModule],
-	declarations: [AppComponent, ItemsComponent, ItemDetailComponent, HomeComponent],
+	imports: [AppRoutingModule],
+	declarations: [AppComponent, HomeComponent],
 	providers: [
 		{ provide: ENDPOINT_URL, useValue: "./" },
 		{ provide: MOCK_MODE, useValue: true },
 	],
-	schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule {}
