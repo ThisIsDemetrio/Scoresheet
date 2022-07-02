1da30d316e90b9ce86ad7a2cea3f57d38953d4db
carbammato <demetrio.marino1985@gmail.com>
2022-07-02 19:08:56 +0200

    add another TODO

--- angular/src/app/components/auth/login/login.component.ts
+++ angular/src/app/components/auth/login/login.component.ts
@@ -1,12 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
 	selector: "app-login",
 	templateUrl: "./login.component.html",
 	styleUrls: ["./login.component.scss"],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 	formShown: "login" | "signup" = "login";
+
+	ngOnInit(): void {
+		// TODO: In case I'm already logged, route me to /home
+	}
 
 	onSignupPageRequested(): void {
 		this.formShown = "signup";
