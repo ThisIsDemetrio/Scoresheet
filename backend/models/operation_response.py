a48886a76219644a61346661a2a84215bc9291bb
carbammato <demetrio.marino1985@gmail.com>
2022-07-17 19:35:06 +0200

    minor fix on Enumerator

--- backend/models/operation_response.py
+++ backend/models/operation_response.py
@@ -2,9 +2,9 @@ import enum
 
 
 class OperationReasonCode(enum.Enum):
-    Success: 1
-    GroupNotEmpty: 2
-    OldPasswordNotValid: 3
+    Success = 1
+    GroupNotEmpty = 2
+    OldPasswordNotValid = 3
 
 
 class OperationResponseModel:
