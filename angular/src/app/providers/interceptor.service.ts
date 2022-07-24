e284ce55ec5d638f547751c750080eaf8447061f
carbammato <demetrio.marino1985@gmail.com>
2022-07-24 23:01:51 +0200

    navigate to login in case of non-valid token

--- angular/src/app/providers/interceptor.service.ts
+++ angular/src/app/providers/interceptor.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from "@angular/core";
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
-
-import { Router } from "@angular/router";
 import { AuthService } from "./auth.service";
+import { Router } from "@angular/router";
 
 @Injectable({
 	providedIn: "root",
 })
 export class InterceptorService implements HttpInterceptor {
-	constructor(private _auth: AuthService) {}
+	constructor(private _auth: AuthService, private readonly router: Router) {}
 
 	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 		if (!request.headers.has("Content-Type")) {
@@ -23,6 +22,7 @@ export class InterceptorService implements HttpInterceptor {
 					Authorization: `Bearer ${accessToken}`,
 				},
 			});
+		else this.router.navigate(["login"]);
 
 		return next.handle(request);
 	}
