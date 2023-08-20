09b5e1c392c31668cfb27b4efb18f938b1ed19c4
carbammato <demetrio.marino1985@gmail.com>
2023-08-20 19:38:58 +0200

    test: more tests in providers

--- angular/src/app/providers/game.service.ts
+++ angular/src/app/providers/game.service.ts
@@ -16,22 +16,22 @@ export class GameService {
 	}
 
 	getGamesByGroupId(groupId: string): Observable<Game[]> {
-		return this.httpClient.get<Game[]>(`${this.endpoint}/GetByGroup/${groupId}`);
+		return this.httpClient.get<Game[]>(`${this.endpoint}GetByGroup/${groupId}`);
 	}
 
 	createGame(game: Game): Observable<boolean> {
-		return this.httpClient.post<boolean>(`${this.endpoint}/Create`, {
+		return this.httpClient.post<boolean>(`${this.endpoint}Create`, {
 			game,
 		});
 	}
 
 	updateGame(game: Game): Observable<boolean> {
-		return this.httpClient.put<boolean>(`${this.endpoint}/Update/${game.id}`, {
+		return this.httpClient.put<boolean>(`${this.endpoint}Update/${game.id}`, {
 			game,
 		});
 	}
 
 	deleteGame(gameId: string): Observable<boolean> {
-		return this.httpClient.delete<boolean>(`${this.endpoint}/Delete/${gameId}`);
+		return this.httpClient.delete<boolean>(`${this.endpoint}Delete/${gameId}`);
 	}
 }
